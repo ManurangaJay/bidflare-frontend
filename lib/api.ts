b94d5cf7928507e0024b953910980666b72cfee4
Manuranga <manurangajayawardhana@gmail.com>
2025-07-12 13:01:33 +0530

    feat: create a helper function to get base URL of the backend

---
+++ lib/api.ts
@@ -0,0 +1,4 @@
+export const getApiUrl = (path: string): string => {
+  const base = process.env.NEXT_PUBLIC_API_BASE_URL || "";
+  return `${base.replace(/\/+$/, "")}/${path.replace(/^\/+/, "")}`;
+};
